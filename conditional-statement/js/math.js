924b3d6654d97874f1a9694c40d1723e4eb5360d
nicoya7791 <hblandon750@outlook.com>
2021-01-06 11:32:14 -0800

    refator the if statement

--- conditional-statement/js/math.js
+++ conditional-statement/js/math.js
@@ -6,18 +6,16 @@ var message="";
 alert("Let's do some math!");
 
 // collect numeric input
-var num1 = prompt("Please type a number");
+num1 = prompt("Please type a number");
 num1 = parseFloat(num1);
-if(num1==0 || isNaN(num1)){
-    alert("you need to enter a number higher than 0 or enter a number");
-};
-var num2 = prompt("Please type another number");
-num2 = parseFloat(num2)
-if(num2==0|| isNaN(num2)){
-    alert("you need to enter a number higher than 0 or enter a number");
-}
-else{
+num2 = prompt("Please type another number");
+num2 = parseFloat(num2);
+if(num1===0 || isNaN(num1)|| num2===0 || isNaN(num2)){
+    alert("you need to enter a number higher than 0 or enter a number. refresh page");
+}else
+{
 // build an HTML message
+// future refactor remove all concatination and use templet literals.
 var message = "<h1>Math with the numbers " + num2 + " and " + num2 + "</h1>";
 message += num2 + " + " + num2 + " = " + (num1 + num2);
 message += "<br>";
@@ -29,7 +27,7 @@ message += num1 + " - " + num2 + " = " + (num1 - num2);
   
 // write message to web page
 document.write(message);
-}
+};
 
 
 
